125fc69cbc5727f1f0b622692a3be8064ef3e01b
Yon Taek Chung <yontaekc@gwu.edu>
2018-10-03 11:43:10 -0700

    changed front-end to fit the data structure

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { hot } from 'react-hot-loader';
-import headerStyle from './styles/header.css';
-import NavBarStyle from './styles/NavBar.css';
-import PlayButton from './PlayButton.jsx';
-import FollowButton from './FollowButton.jsx';
-import EtcButton from './EtcButton.jsx';
-import About from './About.jsx';
-import NavBar from './NavBar.jsx';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { hot } from "react-hot-loader";
+import headerStyle from "./styles/header.css";
+import NavBarStyle from "./styles/NavBar.css";
+import PlayButton from "./PlayButton.jsx";
+import FollowButton from "./FollowButton.jsx";
+import EtcButton from "./EtcButton.jsx";
+import About from "./About.jsx";
+import NavBar from "./NavBar.jsx";
 
 const Header = ({ artist, handleFollowToggle }) => {
   // background styling has to be done in-line.
   const blackBG = {
     //linear-gradient(transparent, black),
-    background: `linear-gradient(transparent -30%, rgb(24,24,24)), url(${artist.artistImages[0]}) no-repeat top`,
-    backgroundSize: 'cover'
+    background: `linear-gradient(transparent -30%, rgb(24,24,24)), url(${
+      artist.artistimages[0]
+    }) no-repeat top`,
+    backgroundSize: "cover"
   };
 
   return (
@@ -22,12 +24,20 @@ const Header = ({ artist, handleFollowToggle }) => {
       <NavBar />
       <div style={blackBG} className={headerStyle.artistPortraitBG}>
         <div className={headerStyle.scrollSectionContainer}>
-          <span className={headerStyle.monthlyListeners}>{artist.followersNumber.toLocaleString('en')} Followers</span>
-          <h1 className={headerStyle.artistName}>{artist.artistName}</h1>
+          <span className={headerStyle.monthlyListeners}>
+            {artist.followersnumber.toLocaleString("en")} Followers
+          </span>
+          <h1 className={headerStyle.artistName}>{artist.artistname}</h1>
           <div className={headerStyle.buttonSection}>
             <PlayButton />
-            <FollowButton artist={artist} handleFollowToggle={handleFollowToggle} />
-            <EtcButton artist={artist} handleFollowToggle={handleFollowToggle} />
+            <FollowButton
+              artist={artist}
+              handleFollowToggle={handleFollowToggle}
+            />
+            <EtcButton
+              artist={artist}
+              handleFollowToggle={handleFollowToggle}
+            />
           </div>
         </div>
       </div>
--- client/src/LocationStat.jsx
+++ client/src/LocationStat.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { hot } from 'react-hot-loader';
-import AboutStyle from './styles/About.css';
-import classNames from 'classnames';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { hot } from "react-hot-loader";
+import AboutStyle from "./styles/About.css";
+import classNames from "classnames";
 
 class LocationStat extends Component {
   constructor(props) {
@@ -15,7 +15,9 @@ class LocationStat extends Component {
     const entryToRender = (location, followerCount) => (
       <div className="locationStat-entry">
         <h5 className="locationStat-location">{location}</h5>
-        <h6 className="locationStat-followerCount">{followerCount.toLocaleString('en')} followers</h6>
+        <h6 className="locationStat-followerCount">
+          {followerCount.toLocaleString("en")} followers
+        </h6>
       </div>
     );
     return (
